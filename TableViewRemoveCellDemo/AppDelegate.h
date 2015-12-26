4f106d8432cbe924563621d34fe97af45421e39c
wangwendong <wangwendong@wm-mac-mini.local>
2015-12-26 09:53:36 +0800

    Initial Commit

---
+++ TableViewRemoveCellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewRemoveCellDemo
+//
+//  Created by wangwendong on 15/12/26.
+//  Copyright © 2015年 sunricher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewRemoveCellDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewRemoveCellDemo
+//
+//  Created by wangwendong on 15/12/26.
+//  Copyright © 2015年 sunricher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
